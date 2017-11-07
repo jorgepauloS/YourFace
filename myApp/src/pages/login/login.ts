0a563c0e9d46ff9c97a4ca25294b1da933918762
Adailson-Jose <adailsoncca@hotmail.com.br>
2017-11-07 17:41:47 -0300

    Tela login criada.

--- myApp/src/pages/login/login.ts
+++ myApp/src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CadastroPage } from '../cadastro/cadastro';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the LoginPage page.
@@ -15,11 +17,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+     public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  goToCadastroPage(){
+    this.navCtrl.push(CadastroPage);
+  }
+  goToHome(){
+    this.navCtrl.push(HomePage);
   }
 
 }
