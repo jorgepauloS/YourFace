2e3ce02631fe246ce15770f24039ba7d3524b3df
Adailson-Jose <adailsoncca@hotmail.com.br>
2017-11-07 16:46:00 -0300

    Organição da strutura (pastas) do código.

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -3,35 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { IntroPageModule } from '../pages/intro/intro.module';
+import { IntroPage } from '../pages/intro/intro';
+import { CadastroPage } from '../pages/cadastro/cadastro';
+import { LoginPage } from '../pages/login/login';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    IntroPage,
+    CadastroPage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    IntroPageModule
+    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    IntroPage,
+    CadastroPage,
+    LoginPage
+
   ],
   providers: [
     StatusBar,
--- myApp/src/pages/intro/intro.ts
+++ myApp/src/pages/intro/intro.ts
@@ -1,13 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
-
-/**
- * Generated class for the IntroPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { LoginPage } from '../login/login';
 
 @IonicPage()
 @Component({
@@ -26,7 +20,8 @@ export class IntroPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad IntroPage');
   }
-  goToMenu(){
-    this.navCtrl.push(TabsPage)
+
+  goToLoginPage() {
+    this.navCtrl.push(LoginPage);
   }
 }
