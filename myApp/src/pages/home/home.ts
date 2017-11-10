8e90a6bfc4e4b6b77c8f39d5bfadd497b50a0ffc
Adailson-Jose <adailsoncca@hotmail.com.br>
2017-11-10 17:09:33 -0300

    Melhoramento da tela lojin, e criação de um menu na tala home.

--- myApp/src/pages/home/home.ts
+++ myApp/src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, AlertController } from 'ionic-angular';
+import { CadastroPage } from '../cadastro/cadastro';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  user:any [];
+
+  constructor(
+    public navCtrl: NavController,
+    public alertHomeCtrl:AlertController,
+    
+  ) {
 
   }
-
+  showAlert() {
+    let alert = this.alertHomeCtrl.create({
+      title: 'Falta implementar função de SAIR!',
+      subTitle: 'Quem pode fazer isso?',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 }
