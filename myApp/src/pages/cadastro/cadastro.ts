bd2e1fb4679c216ae9536a0da4772de0015d7aad
Adailson-Jose <adailsoncca@hotmail.com.br>
2017-11-09 21:34:54 -0300

    Melhoramento da tela cadastro e função cadastrar "criada" em parte.

--- myApp/src/pages/cadastro/cadastro.ts
+++ myApp/src/pages/cadastro/cadastro.ts
@@ -10,10 +10,61 @@ import { HomePage } from '../home/home';
 })
 export class CadastroPage {
 
+  public dados = {
+    nomeUsuario : null,
+    cpf : null,
+    senha : null,
+    senhaConf : null,
+    email : null,
+    emailConf: null,
+    idade : null,
+
+  };
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public alertCadastroCtrl: AlertController ) {
+  }
+
+  public fazerCadastro(): void {
+    // Pega as informações do usuário
+    var nomeUsuario = this.dados.nomeUsuario;
+    var cpf = this.dados.cpf;
+    var email = this.dados.email;
+    var emailConf = this.dados.emailConf;
+    var senha = this.dados.senha;
+    var SenhaConf = this.dados.senhaConf;
+    var idade = this.dados.idade;
+
+
+    // Compara se as senhas digitadas são correspondentes
+    if (senha !== SenhaConf) {
+      senha.innerText = '';
+      SenhaConf.innerText = '';
+      senha.focus();
+      alert('As senhas não são iguais.')
+      return;
+    }
+
+    // Compara se os e-mails digitados são correspondentes
+    if (email !== emailConf) {
+      email.innerText = '';
+      emailConf.innerText = '';
+      email.focus();
+      alert('E-mails não são iguais.');
+      return;
+    }
+    
+    // Cria o objeto usuario e o cadastro no BD
+    var usuario: object = {
+      username: nomeUsuario,
+      password: senha,
+      email: email
+    };
+    //Falta integrarco o banco.
+    //this.usuarioDAO.cadastrar(usuario);
+
+    this.navCtrl.push(LoginPage);
   }
 
   ionViewDidLoad() {
