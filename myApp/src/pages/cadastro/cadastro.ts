8c1d880441db2c5355a8c53fcec2c49b81a059f3
Adailson-Jose <adailsoncca@hotmail.com.br>
2017-11-20 21:36:36 -0300

    Validações mai smple ok da pagina cadastro do diretor.

--- myApp/src/pages/cadastro/cadastro.ts
+++ myApp/src/pages/cadastro/cadastro.ts
@@ -9,24 +9,41 @@ import { HomePage } from '../home/home';
   templateUrl: 'cadastro.html',
 })
 export class CadastroPage {
+  TestaCPF(strCPF) {
+    let Soma;
+    let Resto;
+    Soma = 0;
+    if (strCPF == "00000000000") return false;
+
+    for (let i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+    Soma = 0;
+    for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+    return true;
+  }
 
   public dados = {
-    nomeUsuario : null,
-    cpf : null,
-    senha : null,
-    senhaConf : null,
-    email : null,
+    nomeUsuario: null,
+    cpf: null,
+    senha: null,
+    senhaConf: null,
+    email: null,
     emailConf: null,
-    idade : null,
-
   };
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public alertCadastroCtrl: AlertController ) {
+    public alertCadastroCtrl: AlertController) {
   }
-
-  public fazerCadastro(): void {
+  fazerCadastro(): boolean {
     // Pega as informações do usuário
     var nomeUsuario = this.dados.nomeUsuario;
     var cpf = this.dados.cpf;
@@ -34,24 +51,47 @@ export class CadastroPage {
     var emailConf = this.dados.emailConf;
     var senha = this.dados.senha;
     var SenhaConf = this.dados.senhaConf;
-    var idade = this.dados.idade;
 
+    if (nomeUsuario == undefined) {
+      alert('O login é um campo obrigatório.');
+      return;
+    }
+    if(cpf == undefined) {
+      alert('O CPF é um campo obrigatório.');
+      return;
+    }
+    if (email == undefined) {
+      alert('O e-mail é um campo obrigatório.');
+      return;
+    }
+    if (emailConf == undefined) {
+      alert('O e-mail de confimação é um campo obrigatório.');
+      return;
+    }
+    if (senha == undefined) {
+      alert('A senha é um campo obrigatório.');
+      return;
+    }
+    if (SenhaConf == undefined) {
+      alert('A senha de confimação é um campo obrigatório.');
+      return;
+    }
+    if (senha.length < 8) {
+      alert('A senha deve ter pelo menos "8" caracteres.');
+      return;
+    }
 
-    // Compara se as senhas digitadas são correspondentes
     if (senha !== SenhaConf) {
-      senha.innerText = '';
-      SenhaConf.innerText = '';
-      senha.focus();
       alert('As senhas não são iguais.')
       return;
     }
 
-    // Compara se os e-mails digitados são correspondentes
     if (email !== emailConf) {
-      email.innerText = '';
-      emailConf.innerText = '';
-      email.focus();
       alert('E-mails não são iguais.');
+      return;
+    }
+    if (this.TestaCPF(cpf)==false) {
+      alert('Cpf inválido.');
       return;
     }
     
@@ -62,20 +102,28 @@ export class CadastroPage {
       email: email
     };
     //Falta integrarco o banco.
-    //this.usuarioDAO.cadastrar(usuario);
-
-    this.navCtrl.push(LoginPage);
+    /*if (this.usuarioDAO.getUser()){
+      this.usuarioDAO.cadastrar(usuario);
+      return true;
+    }*/
+  
+    return true;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CadastroPage');
   }
-  goToLoginPage(dados){
-    console.log(dados)
-    this.showAlert()
-    this.navCtrl.push(LoginPage);
+  goToLoginPage(dados) {
+    if (this.fazerCadastro()){
+      console.log(dados)
+      this.showAlert()
+      this.navCtrl.push(LoginPage);
+    }else{
+      console.log("Algun campo no cadastro está errado!")
+    }
+
   }
-  goToLoginPage2(){
+  goToLoginPage2() {
     this.navCtrl.push(LoginPage);
   }
 
