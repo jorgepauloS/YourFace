280565ad85a098bf699b2d41945a8688a86f74eb
Erico Andre <ericoandresilva@gmail.com>
2017-11-23 07:42:07 -0300

    API de manipulação da base de dados
    modulos usados (express, mysql, body-parse). antes de rodar executar npm install && npm run build,

---
+++ APIServer/src/dao/index.js
@@ -0,0 +1,71 @@
+import  mysql from 'mysql';
+
+const conexao = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	database: "yourface",
+	password : ''
+});
+conexao.findAll = (tabela, callback) =>{
+	let data;
+	conexao.query("SELECT * FROM "+tabela, function (err, result, fields) {
+		let data;
+		if (err)
+			return callback(err);
+		else{
+			return callback(result);
+		}
+	});
+}
+conexao.findOne = (tabela, campo, codigo, callback) =>{
+	let data = {};
+	codigo = parseInt(codigo);
+	if (codigo) {
+		conexao.query('SELECT * FROM '+tabela+' WHERE '+campo+' = ?', [codigo], function (err, result, fields) {
+			if (err)
+				return callback(err);
+			else{
+				return callback(result[0]);
+			}
+		});
+	}
+}
+conexao.login = (tabela, data ,callback) =>{
+	const {cpf, password} = data;
+	const sql = "SELECT * FROM "+tabela+" WHERE cpf=? and password=?";
+	conexao.query(sql, [cpf, password], (err, result) => {
+		if (err)
+			return callback({msg:"erro"});
+		else{
+			if (result[0]) {
+				return callback({msg:"Logado", token: cpf});
+			}else{
+				return callback({msg:"erro"});
+			}
+		}
+	});
+}
+
+conexao.create = (tabela, data, callback) => {
+	const {cpf, name, email, password} = data;
+	const sql = "INSERT INTO " + tabela +" (cpf, name, email, password) VALUES (?,?,?,?)";
+	conexao.query(sql, [cpf, name, email, password], (err, result) => {
+		if (err)
+			return callback({ msg: "erro", error: true });
+		else {
+			return callback({ msg: "Cadastro Realizado", error: false });
+		}
+	});
+}
+conexao.createAluno = (tabela, data, callback) => {
+	const { cpf, name, email } = data;
+	const sql = "INSERT INTO " + tabela + " (cpf, name, email) VALUES (?,?,?)";
+	conexao.query(sql, [cpf, name, email], (err, result) => {
+		if (err)
+			return callback({ msg: "erro", error: true });
+		else {
+			return callback({ msg: "Cadastro Realizado", error: false });
+		}
+	});
+}
+export default conexao;
